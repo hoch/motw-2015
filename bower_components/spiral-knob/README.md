24dbc23c80238647f874359ce4f41810aaf08afa
Hongchan Choi <hongchan@chromium.org>
2015-09-02 15:45:21 -0700

    Update 2015-09-02T22:45:19.366Z

---
+++ bower_components/spiral-knob/README.md
@@ -0,0 +1,10 @@
+# spiral-knob
+
+- For Polymer 1.0
+
+Renders a knob (circular UI widget for parameter control) which can be bound to AudioParam object.
+
+To install, use bower:
+~~~
+bower install spiral-knob
+~~~
---
+++ sampler-step-1.js
@@ -0,0 +1,12 @@
+/**
+ * Step 1
+ * - loadAudioFile from sound directory
+ * - bufferSource -> filter -> amp ADSR enveloping
+ *
+ * Step 2
+ * - multiple buffer sources (samplePlayer: filter + ADSR)
+ * - drum sampler + keyboards
+ *
+ * Step 3
+ * - notification
+ */
